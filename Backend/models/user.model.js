a0bf6c8d11e00fa74bf8bdb6f8f039ab2e3bc364
SupragyaBasnet <125958893+SupragyaBasnet@users.noreply.github.com>
2024-12-28 15:35:48 +0545

    user_model

---
+++ Backend/models/user.model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [3, 'First name must be at least 3 characters long'],
+        },
+        lastname: {
+            type: String,
+            minlength: [3, 'Last name must be at least 3 characters long'],
+        }
+    },
+    phonenumber: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\d{10}$/, 'Phone number must be exactly 10 digits'],  // Regex for 10 digits
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    socketId: {
+        type: String,
+    },
+});
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+    return token;
+};
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+};
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
