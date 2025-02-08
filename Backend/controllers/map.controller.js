d0b1be3475830bf3329ddcba1a060dfb50f50ba2
SupragyaBasnet <125958893+SupragyaBasnet@users.noreply.github.com>
2025-02-08 21:05:30 +0545

    changes in map controller

--- Backend/controllers/map.controller.js
+++ Backend/controllers/map.controller.js
@@ -37,24 +37,6 @@ module.exports.getDistanceTime = async (req, res, next) => {
   }
 };
 
-// module.exports.getAutoCompleteSuggestions = async (req, res, next) => {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return res.status(400).json({ errors: errors.array() });
-//   }
-
-//   const { input } = req.query;
-
-//   try {
-//     const suggestions = await mapService.getAutoCompleteSuggestions(input);
-//     res.status(200).json(suggestions);
-//   } catch (err) {
-//     console.error(err);
-//     res
-//       .status(500)
-//       .json({ message: "Failed to fetch suggestions", error: err.message });
-//   }
-// };
 module.exports.getAutoCompleteSuggestions = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
